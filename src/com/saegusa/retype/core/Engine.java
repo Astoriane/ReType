218730668085160fb55911b02f3d13a4c72eba08
Astoriane <egetrpan@gmail.com>
2016-08-31 00:38:28 +0300

    swag

--- src/com/saegusa/retype/core/Engine.java
+++ src/com/saegusa/retype/core/Engine.java
@@ -1,11 +1,37 @@
 package com.saegusa.retype.core;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Engine {
 	
 	private Session session;
 	
+	private List<String> filesLoaded;
+	
 	public Engine(Session session) {
 		this.session = session;
+	}
+	
+	public void rename() {
+		
+	}
+	
+	public void renameFile(String file, String name) {
+		
+		
+		
+	}
+	
+	public void loadFile(String file) {
+		if (filesLoaded == null)
+			filesLoaded = new ArrayList<String>();
+		
+		if(new File(file).exists())
+			filesLoaded.add(file);
+		else
+			log("File does not exist!");
 	}
 	
 	public Session getSession() {
